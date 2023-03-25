ae318a5d467d3dfbd24a1c09fccccd0e586cfece
icaro <joseicaro.e.a@gmail.com>
2023-03-25 11:05:49 -0300

    1 commit

---
+++ js_menu.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    var botao = $('.bt1');
+    var dropDown = $('.ul-cadastro');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+   
+   $(document).ready(function() {
+    var botao = $('.bt2');
+    var dropDown = $('.ul-consultas');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+   
+   $(document).ready(function() {
+    var botao = $('.bt3');
+    var dropDown = $('.ul-promissoria');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+   
+   $(document).ready(function() {
+    var botao = $('.bt4');
+    var dropDown = $('.ul-rotinas');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+   
+   $(document).ready(function() {
+    var botao = $('.bt5');
+    var dropDown = $('.ul-senha');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
