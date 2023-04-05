b73f3b0320b162e716aac1e60fe62fece07ba1b8
Shalon Pereira dos Santos <shalon.santos@odontosystem.com.br>
2023-04-05 19:48:49 -0300

    commit controle - chamando estoque e chamando demais

--- js/menu-shalom.js
+++ js/menu-shalom.js
@@ -3,22 +3,40 @@ var CadClientePJ = document.querySelector('.CadClientePJ');
 var Estoque = document.querySelector('.Box');
 
 function abriPF() {
-    if(CadClientePF.style.display == "none")
+    if(CadClientePF.style.display == "none"){
+        Estoque.style.display = 'none';
+        CadClientePJ.style.display = 'none';
         CadClientePF.style.display = 'block';
-    else
+    }
+    else{
+        Estoque.style.display = 'none';
+        CadClientePJ.style.display = 'none';
         CadClientePF.style.display = 'block';
+    }
 }
 
 function abriPJ() {
-    if(CadClientePJ.style.display == "none")
+    if(CadClientePJ.style.display == "none"){
+        CadClientePF.style.display = 'none';
+        Estoque.style.display = 'none';        
         CadClientePJ.style.display = 'block';
-    else
+    }
+    else{
+        CadClientePF.style.display = 'none';
+        Estoque.style.display = 'none';    
         CadClientePJ.style.display = 'block';
+    }
 }
 
 function getEstoque() {
-    if(Estoque.style.display == "none")
-    Estoque.style.display = 'block';
-    else
-    Estoque.style.display = 'block';
+    if(Estoque.style.display == "none"){
+        CadClientePF.style.display = 'none';
+        CadClientePJ.style.display = 'none';
+        Estoque.style.display = 'block';
+    }
+    else{
+        CadClientePF.style.display = 'none';
+        CadClientePJ.style.display = 'none';    
+        Estoque.style.display = 'block';
+    }
 }
