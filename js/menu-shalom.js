6cea09602ef0c69151a3bf74bb2729da39eda8c5
Shalon Pereira dos Santos <shalon.santos@odontosystem.com.br>
2023-04-10 20:35:21 -0300

    commit controle - reformulando

--- js/menu-shalom.js
+++ js/menu-shalom.js
@@ -1,55 +1,126 @@
+// VARIEAVES QUE RECEBEM A DIV REFERENTE AS "TELAS"
 var CadClientePF = document.querySelector('.CadClientePF');
 var CadClientePJ = document.querySelector('.CadClientePJ');
 var Estoque = document.querySelector('.Estoque');
+var searchClient = document.querySelector('.search-client');
+var consultaPedidos = document.querySelector('.consulta-pedidos');
 
+//OPEN - CLOSE
 function getCadClientePF() {
     if(CadClientePF.style.display == "none"){
+
+        CadClientePJ.style.display = 'none'; 
         Estoque.style.display = 'none';
-        CadClientePJ.style.display = 'none';
+        searchClient.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+
         CadClientePF.style.display = 'block';
+
     }
     else{
+        CadClientePJ.style.display = 'none'; 
         Estoque.style.display = 'none';
-        CadClientePJ.style.display = 'none';
+        searchClient.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+
         CadClientePF.style.display = 'block';
     }
 }
 function closePF() {
     CadClientePF.style.display = 'none';
 }
-
+//OPEN - CLOSE
 function getCadClientePJ() {
     if(CadClientePJ.style.display == "none"){
+
+        Estoque.style.display = 'none';
+        searchClient.style.display = 'none';
+        consultaPedidos.style.display = 'none';
         CadClientePF.style.display = 'none';
-        Estoque.style.display = 'none';        
+
         CadClientePJ.style.display = 'block';
     }
     else{
-        CadClientePF.style.display = 'none';
-        Estoque.style.display = 'none';    
+        Estoque.style.display = 'none';
+        searchClient.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+        CadClientePF.style.display = 'block';
+
         CadClientePJ.style.display = 'block';
     }
 }
 function closePJ() {
     CadClientePJ.style.display = 'none';
 }
-
+//OPEN - CLOSE
 function getEstoque() {
     if(Estoque.style.display == "none"){
+
+        CadClientePJ.style.display = 'none'; 
+        searchClient.style.display = 'none';
+        consultaPedidos.style.display = 'none';
         CadClientePF.style.display = 'none';
-        CadClientePJ.style.display = 'none';
+
         Estoque.style.display = 'block';
     }
     else{
-        CadClientePF.style.display = 'none';
-        CadClientePJ.style.display = 'none';    
+        CadClientePJ.style.display = 'none'; 
+        searchClient.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+
         Estoque.style.display = 'block';
     }
 }
 function closeEstoque() {
     Estoque.style.display = 'none';
 }
+//OPEN - CLOSE
+function getSearchClient() {
+    if(searchClient.style.display == "none"){
 
+        CadClientePJ.style.display = 'none'; 
+        Estoque.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+        CadClientePF.style.display = 'none';
+
+        searchClient.style.display = 'block';
+    }
+    else{
+        CadClientePJ.style.display = 'none'; 
+        Estoque.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+        CadClientePF.style.display = 'none';
+        
+        searchClient.style.display = 'block';
+    }
+}
+function closeSearchClient() {
+    searchClient.style.display = 'none';
+}
+//OPEN - CLOSE
+function getConsultaPedidos() {
+    if(searchClient.style.display == "none"){
+
+        CadClientePJ.style.display = 'none'; 
+        Estoque.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+        CadClientePF.style.display = 'none';
+        
+        searchClient.style.display = 'block';
+    }
+    else{
+        CadClientePJ.style.display = 'none'; 
+        Estoque.style.display = 'none';
+        consultaPedidos.style.display = 'none';
+        CadClientePF.style.display = 'none';
+        
+        searchClient.style.display = 'block';
+    }
+}
+function closeConsultaPedidos() {
+    consultaPedidos.style.display = 'none';
+}
+//OPEN - CLOSE
 function getMenu() {
     window.location = '/views/Menu-shalom.html';
 }
