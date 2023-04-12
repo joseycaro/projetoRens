a25165dc43ff90ee9c181bfda998e1a199c0b583
icaro <joseicaro.e.a@gmail.com>
2023-04-12 14:54:55 -0300

    feito o começo da ligação do banco

--- Table/pythonsql/pythonsql.py
+++ Table/pythonsql/pythonsql.py
@@ -1,1 +1,8 @@
-import pyodbc
+import pandas as pd
+import psycopg2 as pg
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://postgres:123456@localhost:5435/DBRensClint')
+
+
+
